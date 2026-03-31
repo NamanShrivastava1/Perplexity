476fce9053122cf6c77e3c1ae9ed8f9172e78325
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-03-31 23:16:35 +0530

    Register and Verification Done

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import userModel from "../models/user.model.js";
 import { sendEmail } from "../services/mail.service.js";
+import jwt from "jsonwebtoken";
 
 export async function register(req, res) {
   const { username, email, password } = req.body;
@@ -21,14 +22,21 @@ export async function register(req, res) {
       username,
       email,
       password,
-      verified: false,
     });
+
+    const emailVerificationToken = jwt.sign(
+      {
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+    );
 
     await sendEmail(
       user.email,
       "Welcome to Perplexity!",
-      `<h1>Welcome, ${user.username}!</h1><p>Thank you for registering at Perplexity. We're excited to have you on board!</p>`,
-      `Welcome, ${user.username}! Thank you for registering at Perplexity. We're excited to have you on board!`,
+      `<h1>Welcome, ${user.username}!</h1><p>Thank you for registering at Perplexity. We're excited to have you on board!</p>
+      <p>Please verify your email address by clicking the link below:</p><p><a href="http://localhost:3000/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a></p>
+      <p>Thank you,<br/>The Perplexity Team</p>`,
     );
 
     res.status(201).json({
@@ -48,4 +56,28 @@ export async function register(req, res) {
       error: error.message,
     });
   }
+}
+
+export async function verifyEmail(req, res) {
+  const { token } = req.query;
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  const user = await userModel.findOne({ email: decoded.email });
+
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "Invalid token or user not found",
+      error: "User Not Found",
+    });
+  }
+
+  user.verified = true;
+  await user.save();
+
+  const html = `<h1>Email Verified</h1><p>Your email has been verified successfully. You can now log in to your account.</p>
+  <p><a href="http://localhost:3000/login">Go to Login</a></p>`;
+
+  res.send(html);
 }
