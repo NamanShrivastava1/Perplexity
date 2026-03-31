27dfc24aaa7e9ad31082dfe2948af614a7b256c0
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-04-01 00:03:14 +0530

    Implement Langchain

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import "dotenv/config";
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
+import { testAi } from "./src/services/ai.service.js";
+testAi();
 
 connectDB();
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
